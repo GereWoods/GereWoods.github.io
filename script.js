35706d3a1c9cd841bcf145c0bf545088a4685051
G.Edison <g.edison.code@gmail.com>
2025-05-23 05:11:11 -0300

    Update

--- script.js
+++
@@ -1,8 +0,0 @@
-  document.getElementById("copy-email").addEventListener("click", function(event) {
-    event.preventDefault(); 
-    const hrefText = this.getAttribute("href");
-
-    navigator.clipboard.writeText(hrefText)
-      .then(() => alert("Email copied to clipboard!"))
-      .catch(err => console.error("Error al copiar:", err));
-  });
